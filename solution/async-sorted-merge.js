002ead5e88b173e110a8e4b1e785229ef15faab2
Julia Hazer <juliahazer@yahoo.com>
2017-09-05 14:08:33 -0700

    adding solutions for sync-sorted-merge and async-sorted-merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,58 @@
 'use strict'
 
+/*3rd party module https://www.npmjs.com/package/binary-search-insert, used to more efficiently insert a log chronologically by last.date into the sorted logSources array: O(log(n) using binary search/insert*/
+let binarySearchInsert = require('binary-search-insert');
+let comparator = function (a, b) { return b.last.date - a.last.date; }
+
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+  /*sort the logSources reverse chronologically using mergesort, based on the last log (which is the oldest w/in each logSource, since each source is already chronological) */
+  logSources = mergesort(logSources);
+
+  let curr;
+
+  //while there are still logs to process...
+  while (logSources.length > 0) {
+    /* remove the last logSources in the array (last is the oldest date) & print it */
+    curr = logSources.pop();
+    printer.print(curr.last);
+    //remove that log from the logSources & reset last
+    /******NOTE: I didn't not use Promise.then or Promise.all(iterable) or wait for the Promise to resolve, since I had direct access to LogSource.last - as it was set prior to the creation of the returned Promise */
+    curr.popAsync();
+    //if the logSource isn't drained, then insert the new last log into logSources using binarySearchInsert
+    if (!curr.drained) {
+      binarySearchInsert(logSources, comparator, curr);
+    }
+  }
+
+  //prints a few stats (i.e., logs printed, time taken, logs/s) of the solution
+  printer.done();
+
+  /*Big O: time complexity for mergesort is on average O(n log(n)). Source: http://bigocheatsheet.com/ */
+  function mergesort(input) {
+    function merge(arr1, arr2){
+      const totalElements = arr1.length + arr2.length;
+      let result = [];
+      let i = 0;
+      let j = 0;
+      while (i + j < totalElements) {
+        //sort chronogically by last.date (oldest at the end)
+        if (j >= arr2.length || (i < arr1.length && arr1[i].last.date >= arr2[j].last.date)){
+          result.push(arr1[i]);
+          i++;
+        } else {
+          result.push(arr2[j]);
+          j++;
+        }
+      }
+      return result;
+    }
+
+    function divide(arr) {
+      if (arr.length < 2) { return arr; }
+      const mid = Math.floor(arr.length / 2);
+      return merge(divide(arr.slice(0, mid)), divide(arr.slice(mid)));
+    }
+
+    return divide(input);
+  }
 }
