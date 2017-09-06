f7944f98216609c520884c17455c9da193de0841
Julia Hazer <juliahazer@yahoo.com>
2017-09-06 11:33:28 -0700

    refactored to solely use BinarySearchInsert (removed mergeSort) & use .popAsync (recursion)

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,58 +1,38 @@
 'use strict'
 
 /*3rd party module https://www.npmjs.com/package/binary-search-insert, used to more efficiently insert a log chronologically by last.date into the sorted logSources array: O(log(n) using binary search/insert*/
-let binarySearchInsert = require('binary-search-insert');
-let comparator = function (a, b) { return b.last.date - a.last.date; }
+const BinarySearchInsert = require('binary-search-insert');
+const comparator = function (a, b) { return b.last.date - a.last.date; };
 
 module.exports = (logSources, printer) => {
-  /*sort the logSources reverse chronologically using mergesort, based on the last log (which is the oldest w/in each logSource, since each source is already chronological) */
-  logSources = mergesort(logSources);
+  /*sort the logSources reverse chronologically using BinarySearchInsert for each logSource, based on the last log (which is the oldest w/in each logSource, since each source is already chronological) */
+  logSources = logSources.reduce((acc, logSource) => {
+    BinarySearchInsert(acc, comparator, logSource);
+    return acc;
+  }, []);
 
   let curr;
 
-  //while there are still logs to process...
-  while (logSources.length > 0) {
-    /* remove the last logSources in the array (last is the oldest date) & print it */
+  processNextLog();
+
+  function processNextLog() {
+    //base case - print results & return if no logs left to process
+    if (logSources.length === 0) {
+      //prints a few stats (i.e., logs printed, time taken, logs/s) of the solution
+      printer.done();
+      return;
+    }
+    /* otherwise, remove the last logSources in the array (last is the oldest date) & print it */
     curr = logSources.pop();
     printer.print(curr.last);
-    //remove that log from the logSources & reset last
-    /******NOTE: I didn't not use Promise.then or Promise.all(iterable) or wait for the Promise to resolve, since I had direct access to LogSource.last - as it was set prior to the creation of the returned Promise */
-    curr.popAsync();
-    //if the logSource isn't drained, then insert the new last log into logSources using binarySearchInsert
-    if (!curr.drained) {
-      binarySearchInsert(logSources, comparator, curr);
-    }
-  }
-
-  //prints a few stats (i.e., logs printed, time taken, logs/s) of the solution
-  printer.done();
-
-  /*Big O: time complexity for mergesort is on average O(n log(n)). Source: http://bigocheatsheet.com/ */
-  function mergesort(input) {
-    function merge(arr1, arr2){
-      const totalElements = arr1.length + arr2.length;
-      let result = [];
-      let i = 0;
-      let j = 0;
-      while (i + j < totalElements) {
-        //sort chronogically by last.date (oldest at the end)
-        if (j >= arr2.length || (i < arr1.length && arr1[i].last.date >= arr2[j].last.date)){
-          result.push(arr1[i]);
-          i++;
-        } else {
-          result.push(arr2[j]);
-          j++;
-        }
+    //remove that log from the logSource & reset last. once the promise has resolved, check the return value (i.e., currLast)...
+    curr.popAsync().then(currLast => {
+      //if the logSource isn't drained (i.e., the return value is the last log), then insert the new last log into logSources using binarySearchInsert
+      if (currLast) {
+        BinarySearchInsert(logSources, comparator, curr);
       }
-      return result;
-    }
-
-    function divide(arr) {
-      if (arr.length < 2) { return arr; }
-      const mid = Math.floor(arr.length / 2);
-      return merge(divide(arr.slice(0, mid)), divide(arr.slice(mid)));
-    }
-
-    return divide(input);
+      //recurse until there are no more logs to process...
+      processNextLog();
+    });
   }
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,12 +1,15 @@
 'use strict'
 
 /*3rd party module https://www.npmjs.com/package/binary-search-insert, used to more efficiently insert a log chronologically by last.date into the sorted logSources array: O(log(n) using binary search/insert*/
-let binarySearchInsert = require('binary-search-insert');
-let comparator = function (a, b) { return b.last.date - a.last.date; }
+const BinarySearchInsert = require('binary-search-insert');
+const comparator = function (a, b) { return b.last.date - a.last.date; };
 
 module.exports = (logSources, printer) => {
-  /*sort the logSources reverse chronologically using mergesort, based on the last log (which is the oldest w/in each logSource, since each source is already chronological) */
-  logSources = mergesort(logSources);
+  /*sort the logSources reverse chronologically using BinarySearchInsert for each logSource, based on the last log (which is the oldest w/in each logSource, since each source is already chronological) */
+  logSources = logSources.reduce((acc, logSource) => {
+    BinarySearchInsert(acc, comparator, logSource);
+    return acc;
+  }, []);
 
   let curr;
 
@@ -15,43 +18,14 @@ module.exports = (logSources, printer) => {
     /* remove the last logSources in the array (last is the oldest date) & print it */
     curr = logSources.pop();
     printer.print(curr.last);
-    //remove that log from the logSources & reset last
+    //remove that log from the logSource & reset last
     curr.pop();
-    //if the logSource isn't drained, then insert the new last log into logSources using binarySearchInsert
+    //if the logSource isn't drained, then insert the new last log into logSources using BinarySearchInsert
     if (!curr.drained) {
-      binarySearchInsert(logSources, comparator, curr);
+      BinarySearchInsert(logSources, comparator, curr);
     }
   }
 
   //prints a few stats (i.e., logs printed, time taken, logs/s) of the solution
   printer.done();
-
-  /*Big O: time complexity for mergesort is on average O(n log(n)). Source: http://bigocheatsheet.com/ */
-  function mergesort(input) {
-    function merge(arr1, arr2){
-      const totalElements = arr1.length + arr2.length;
-      let result = [];
-      let i = 0;
-      let j = 0;
-      while (i + j < totalElements) {
-        //sort chronogically by last.date (oldest at the end)
-        if (j >= arr2.length || (i < arr1.length && arr1[i].last.date >= arr2[j].last.date)){
-          result.push(arr1[i]);
-          i++;
-        } else {
-          result.push(arr2[j]);
-          j++;
-        }
-      }
-      return result;
-    }
-
-    function divide(arr) {
-      if (arr.length < 2) { return arr; }
-      const mid = Math.floor(arr.length / 2);
-      return merge(divide(arr.slice(0, mid)), divide(arr.slice(mid)));
-    }
-
-    return divide(input);
-  }
 }
